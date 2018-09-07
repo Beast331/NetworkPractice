1382b29f872e8d7c6c5ca9214647d73f236d0e25
kailash <a>
2018-09-07 21:05:07 +0530

    brobrobrobrobro did some cool stuff bro

---
+++ NetworkPractice_100818/Assets/Scripts/Lobby_Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Lobby_Manager : NetworkLobbyManager {
+
+	public GameObject lobby;
+
+	void Start()
+	{
+		lobby.SetActive (false);
+	}
+	public override void OnStartHost()
+	{
+		base.OnStartHost();
+		Debug.Log ("Game has started lol");
+		lobby.SetActive (true);
+	}
+}
---
+++ NetworkPractice_100818/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour {
+
+	public InputField matchNameInput;
+	public Lobby_Manager lm;
+
+	public void OnClickHost()
+	{
+		lm.StartMatchMaker ();
+		lm.matchMaker.CreateMatch (matchNameInput.text, (uint)lm.maxPlayers, true, "","","",0,0,lm.OnMatchCreate);
+	}
+
+}
